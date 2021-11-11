a333aead3f190c8c9ab3f00392302b2e1e945507
Haoxuan Zou <zouhaoxuan@gmail.com>
2021-11-11 13:27:14 -0500

    Haoxuan Zou

--- setup.py
+++ setup.py
@@ -3,10 +3,7 @@ from distutils.core import setup
 setup(
     name="Haoxuan Zou",
     version="0.1dev",
-    packages=[
-        "economicsl@git+https://github.com/ox-inet-resilience/py-economicsl@master",
-        "numpy",
-    ],
+    packages=["economicsl", "numpy",],
     license="Apache",
     long_description=open("README.txt").read(),
 )
