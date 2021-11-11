57b8ca745376985d29f5689f4188b094ab30897a
Haoxuan Zou <zouhaoxuan@gmail.com>
2021-11-11 13:35:03 -0500

    Haoxuan Zou

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="resilience-CH",
+    name="resilience_CH",
     version="0.1dev",
     packages=find_packages(exclude=["tests"]),
     license="Apache",
